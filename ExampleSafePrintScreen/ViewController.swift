294d06da0404e87a8e4fb58694c9ca241b9ec875
Raphael Torquato <martinitorquato@gmail.com>
2023-05-09 18:38:33 -0300

    Initial Commit

---
+++ ExampleSafePrintScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleSafePrintScreen
+//
+//  Created by Raphael Torquato on 09/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
